033cb7c4eb5a23d391230a16bc092505e1abe9c7
Yi Xiang <tayyixiang.91@gmail.com>
2018-12-22 23:09:54 +0800

    PairRDDs

--- src/main/java/com/yixiangtay/learning/apache/spark/Main.java
+++ src/main/java/com/yixiangtay/learning/apache/spark/Main.java
@@ -1,11 +1,11 @@
 package com.yixiangtay.learning.apache.spark;
 
+import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -19,25 +19,28 @@ public class Main {
         // logging
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
-        List<Integer> inputData = new ArrayList<Integer>();
-        inputData.add(35);
-        inputData.add(12);
-        inputData.add(90);
-        inputData.add(20);
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN: Saturday 8 September 1942");
 
         // local[*] - using all cores
         SparkConf conf = new SparkConf().setAppName("sparkApplication").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // load in a collection and turn it into a RDD
-        JavaRDD<Integer> javaRDD = sc.parallelize(inputData);
-        // map operation
-        // using Java class
-        // JavaRDD<IntegerWithSquareRoot> sqrtRDD = javaRDD.map(value -> new IntegerWithSquareRoot(value));
-        // using Scala tuple
-        JavaRDD<Tuple2<Integer, Double>> sqrtRDD = javaRDD.map(value -> new Tuple2<>(value, Math.sqrt(value)));
-
-        Tuple2<Integer, Double> scalaTuple = new Tuple2<>(9, 3.0);
+        // reduce by key
+        sc.parallelize(inputData)
+                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+                .reduceByKey((value1, value2) -> value1 + value2)
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
+        // group by key
+        // performance issues (not recommended)
+        sc.parallelize(inputData)
+                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+                .groupByKey()
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + Iterables.size(tuple._2) + " instances"));
 
         sc.close();
     }
