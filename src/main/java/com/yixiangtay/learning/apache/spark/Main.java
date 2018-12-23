ed3bc8b546e8c50ead47e60be190f7f83c9b7cd2
Yi Xiang <tayyixiang.91@gmail.com>
2018-12-23 16:24:34 +0800

    Keyword Ranking Practical

--- src/main/java/com/yixiangtay/learning/apache/spark/Main.java
+++ src/main/java/com/yixiangtay/learning/apache/spark/Main.java
@@ -1,13 +1,15 @@
 package com.yixiangtay.learning.apache.spark;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class Main {
 
@@ -24,14 +26,21 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         // read from disk
-        JavaRDD<String> initialRDD = sc.textFile("src/main/resources/subtitles/input.txt");
+        JavaRDD<String> initialRDD = sc.textFile("src/main/resources/subtitles/input-spring.txt");
 
-        // flat map
-        initialRDD
-                .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
-                .filter(word -> word.matches("[a-zA-Z:]+"))
-                .map(value -> value.replace(":", ""))
-                .collect().forEach(System.out::println);
+        // keyword ranking
+        JavaRDD<String> lettersOnlyRDD = initialRDD.map(sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase());
+        JavaRDD<String> removedBlankLines = lettersOnlyRDD.filter(sentence -> sentence.trim().length() > 0);
+        JavaRDD<String> justWords = removedBlankLines.flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator());
+        JavaRDD<String> blankWordsRemoved = justWords.filter(word -> word.trim().length() > 0);
+        JavaRDD<String> justInterestingWords = blankWordsRemoved.filter(Util::isNotBoring);
+        JavaPairRDD<String, Long> pairRDD = justInterestingWords.mapToPair(word -> new Tuple2<>(word, 1L));
+        JavaPairRDD<String, Long> totals = pairRDD.reduceByKey((value1, value2) -> value1 + value2);
+        JavaPairRDD<Long, String> switched = totals.mapToPair(tuple -> new Tuple2<Long, String> (tuple._2, tuple._1));
+        JavaPairRDD<Long, String> sorted = switched.sortByKey(false);
+
+        List<Tuple2<Long, String>> results = sorted.take(10);
+        results.forEach(System.out::println);
 
         sc.close();
     }
