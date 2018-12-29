3b6a5765416fe5a5d2d2291a77b7575a94dbd1a9
Yi Xiang <tayyixiang.91@gmail.com>
2018-12-30 00:00:54 +0800

    The Full SQL Syntax

--- src/main/java/com/yixiangtay/learning/apache/spark/Main.java
+++ src/main/java/com/yixiangtay/learning/apache/spark/Main.java
@@ -2,11 +2,9 @@ package com.yixiangtay.learning.apache.spark;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import static org.apache.spark.sql.functions.col;
 
 public class Main {
 
@@ -36,37 +34,10 @@ public class Main {
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
         dataset.show(10);
 
-        long numberOfRows = dataset.count();
-        System.out.println("There are " + numberOfRows + " records.");
-
-        Row firstRow = dataset.first();
-        // get() returns Object
-        String subjectUsingGet = firstRow.get(2).toString();
-        // getAs() returns String
-        String subjectUsingGetAs = firstRow.getAs("subject");
-        System.out.println("Subject using get(): " + subjectUsingGet);
-        System.out.println("Subject using getAs(): " + subjectUsingGetAs);
-        int year = Integer.parseInt(firstRow.getAs("year"));
-        System.out.println("Year: " + year);
-
-        // filter using expressions
-        Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' AND year >= 2007");
-        modernArtResults.show();
-
-        // filter using lambdas
-        Dataset<Row> modernArtResults2 = dataset.filter(
-                (FilterFunction<Row>) row ->
-                        row.getAs("subject").equals("Modern Art")
-                        && Integer.parseInt(row.getAs("year")) >= 2007);
-        modernArtResults2.show();
-
-        // filter using columns
-        Dataset<Row> modernArtResults3 =
-                dataset.filter(
-                        col("subject").equalTo("Modern Art")
-                        .and(col("year").geq(2007))
-        );
-        modernArtResults3.show();
+        // full sql syntax
+        dataset.createOrReplaceTempView("my_students_view");
+        Dataset<Row> results = spark.sql("SELECT distinct(year) FROM my_students_view WHERE subject='French' ORDER BY year DESC");
+        results.show();
 
         spark.close();
     }
