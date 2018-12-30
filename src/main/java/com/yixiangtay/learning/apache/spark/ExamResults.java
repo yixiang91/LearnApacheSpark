8534f5b526b38ffd7ff1d5d8e5068f41f9998342
Yi Xiang <tayyixiang.91@gmail.com>
2018-12-30 22:28:35 +0800

    User Defined Functions

--- src/main/java/com/yixiangtay/learning/apache/spark/ExamResults.java
+++ src/main/java/com/yixiangtay/learning/apache/spark/ExamResults.java
@@ -1,15 +1,14 @@
 package com.yixiangtay.learning.apache.spark;
 
+import java.text.SimpleDateFormat;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import static org.apache.spark.sql.functions.callUDF;
 import static org.apache.spark.sql.functions.col;
-import static org.apache.spark.sql.functions.mean;
-import static org.apache.spark.sql.functions.round;
-import static org.apache.spark.sql.functions.stddev;
 
 public class ExamResults {
 
@@ -36,14 +35,37 @@ public class ExamResults {
                         .getOrCreate();
 
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
-        Column score = dataset.col("score");
 
-        dataset
-                .groupBy("subject")
-                .pivot("year")
-                .agg(round(mean(col("score")), 2).alias("mean_score"),
-                     round(stddev(col("score")), 2).alias("std_dev"))
-                .show();
+        // user-defined functions (modern-way)
+        // example #1
+        spark.udf().register("hasPassed", (String grade, String subject) -> {
+            if (subject.equals("Biology") && !grade.startsWith("A")) {
+                return false;
+            }
+            return grade.startsWith("A") || grade.startsWith("B") || grade.startsWith("C");
+        }, DataTypes.BooleanType);
+        // using a literal to invoke a UDF (without registering it)
+        // dataset = dataset.withColumn("pass", lit(col("grade").equalTo("A+")));
+        // invoking a registered UDF
+        dataset = dataset.withColumn("pass", callUDF("hasPassed", col("grade"), col("subject")));
+        dataset.show(20);
+
+        // example #2
+        SimpleDateFormat input = new SimpleDateFormat("MMMM");
+        SimpleDateFormat output = new SimpleDateFormat("M");
+        spark.udf().register("monthNum", (String month) -> {
+            java.util.Date inputDate = input.parse(month);
+            return Integer.parseInt(output.format(inputDate));
+        }, DataTypes.IntegerType);
+        Dataset<Row> dataset2 = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
+        dataset2.createOrReplaceTempView("logging_view");
+        Dataset<Row> results = spark.sql(
+                "SELECT level, DATE_FORMAT(datetime, 'MMMM') AS month, COUNT(1) AS total " +
+                        "FROM logging_view " +
+                        "GROUP BY level, month " +
+                        "ORDER BY monthNum(month), level"
+        );
+        results.show();
 
         spark.close();
     }
