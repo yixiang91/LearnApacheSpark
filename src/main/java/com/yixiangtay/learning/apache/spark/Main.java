22beec6195dfaa604445651eb239c3ee32ff381e
Yi Xiang <tayyixiang.91@gmail.com>
2018-12-30 13:46:02 +0800

    DataFrames API

--- src/main/java/com/yixiangtay/learning/apache/spark/Main.java
+++ src/main/java/com/yixiangtay/learning/apache/spark/Main.java
@@ -5,6 +5,9 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.date_format;
 
 public class Main {
 
@@ -30,15 +33,24 @@ public class Main {
                         .config("spark.sql.warehouse.dir", "file:///C:/tmp/")
                         .getOrCreate();
 
-        // ordering
         Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
-        dataset.createOrReplaceTempView("logging_view");
-        Dataset<Row> results = spark.sql(
-                "SELECT level, DATE_FORMAT(datetime, 'MMMM') AS month, COUNT(1) as total " +
-                        "FROM logging_view " +
-                        "GROUP BY level, month " +
-                        "ORDER BY CAST(FIRST(DATE_FORMAT(datetime, 'M')) AS INT), level");
-        results.show(60);
+        // dataset.createOrReplaceTempView("logging_view");
+        // Dataset<Row> results = spark.sql(
+        //         "SELECT level, DATE_FORMAT(datetime, 'MMMM') AS month, COUNT(1) as total " +
+        //                 "FROM logging_view " +
+        //                 "GROUP BY level, month " +
+        //                 "ORDER BY CAST(FIRST(DATE_FORMAT(datetime, 'M')) AS INT), level");
+
+        // dataframes api
+        dataset
+                .select(col("level"),
+                        date_format(col("datetime"), "MMMM").alias("month"),
+                        date_format(col("datetime"), "M").alias("month_num").cast(DataTypes.IntegerType))
+                .groupBy(col("level"), col("month"), col("month_num"))
+                .count()
+                .orderBy(col("month_num"), col("level"))
+                .drop(col("month_num"))
+                .show(60);
 
         spark.close();
     }
